8db29ff8fe87e6a90d6b0d891b3bc2e8abc7340e
swetha <b22in024@kitsw.ac.in>
2025-06-24 11:45:20 +0530

    new file

---
+++ Calculator.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import java.awt.event.*;
+
+class SimpleCal(){
+	
+	SimpleCal(){
+		JFrame frame = new JFrame("Cal");
+		frame.setSize(300,300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Frame.setLayout(null);
+		frame.setVisible(true);
+		
+		JTextField input1 = new JTextField(10);
+		JTextField input2 = new JTextField(10);
+		JTextField input3 = new JTextField();
+		
+		JButton plus = new JButton("+");
+		
+		plus.addActionListener(
+		new ActionListener(){
+			public voiid actionPerformed(ActionEvent e){
+				
+		}
+		});
+		   
